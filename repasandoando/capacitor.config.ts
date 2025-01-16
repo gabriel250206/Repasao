b95b351965b26214b0cd482111c9d2ca96bf417c
gabriel250206 <158100593+gabriel250206@users.noreply.github.com>
2025-01-16 08:48:51 -0600

    primer commit

---
+++ repasandoando/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'repasandoando',
+  webDir: 'www'
+};
+
+export default config;
